f4af27d058cd239cb32cab3dd8886828ab1914c9
Kristian Kubis <kubis98.k@gmail.com>
2022-02-12 10:07:54 +0100

    finished header + brands start

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -13,15 +13,14 @@ const Header = () => {
         <input typeof='email' placeholder='Your email address'/>
         <button type='button'>Get Started</button>
       </div>
-      <div className='gpt3__header__content__people'>
+      <div className='gpt3__header-content__people'>
         <img src={people} alt="people"/>
         <p>1,600 people requested access a visit in last 24 hours</p>
       </div>
-
-      {/* <div className='gpt3___header-image'>
-        <img src={ai} alt="ai"/>
-      </div> */}
     </div>
+    <div className='gpt3___header-image'>
+        <img src={ai} alt="ai"/>
+      </div>
    
 
     </div>
