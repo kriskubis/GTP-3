37cb4ba46b3121adc0ec3b1fa1cec71c94312711
Kristian Kubis <44458501+kriskubis@users.noreply.github.com>
2022-02-12 23:22:56 +0100

    what section start

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './whatGPR3.css';
 const WhatGPR3 = () => {
   return (
-    <div>WhatGPR3</div>
+    <div className="gpt3__whatgpt3 section__margin" id='whgpt3'>
+      <div className="gpt3__whatgpt3-feature">
+        <Feature/>
+        </div>
+        <div className="gpt3__whatgpt3-heading">
+          <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+          <p>Explore The Library</p>
+        </div>
+        <div className="gpt3__whatgpt3-container">
+          <Feature/>
+          <Feature/>
+          <Feature/>
+        </div>
+      </div>
+    
   )
 }
 
