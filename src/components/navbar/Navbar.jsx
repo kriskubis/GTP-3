22f260cde14dc4d71ad4d0ef266ece42d75cc171
Kristian Kubis <44458501+kriskubis@users.noreply.github.com>
2022-02-15 16:10:43 +0100

    Finished Features, Possibility, CTA, Blog, Footer

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import './navbar.css';
 const Menu = () => (
   <>
     <p><a href='#home'>Home</a></p>
-    <p><a href='#whatGPT3'>What is GPT-3?</a></p>
+    <p><a href='#whgpt3'>What is GPT-3?</a></p>
     <p><a href='#possibility'>Open AI</a></p>
     <p><a href='#features'>Case Studies</a></p>
     <p><a href='#blog'>Library</a></p>
